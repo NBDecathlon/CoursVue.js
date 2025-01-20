65746331d53052cac029b259636d3120976ec054
Nassim Bentifraouine <nassim.bentifraouine@decathlon.com>
2025-01-20 14:33:49 +0100

    commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <product-list :products="products" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import ProductList from './components/ProductList.vue';
+
+interface Product {
+  id: number;
+  label: string;
+  description: string;
+  qte: number;
+}
+
+const products = ref<Product[]>([
+  { id: 1, label: 'Dragon Ball', description: 'Vegeta', qte: 15 },
+  { id: 2, label: 'Naruto Shippuden', description: 'Sasuke', qte: 0 },
+  { id: 3, label: 'Solo Leveling', description: 'Sun Hue', qte: 3 },
+]);
+</script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,54 @@
+<template>
+  <table border="1">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Label</th>
+        <th>Description</th>
+        <th>Statut</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="product in products" :key="product.id">
+        <td>{{ product.id }}</td>
+        <td>{{ product.label }}</td>
+        <td>{{ product.description }}</td>
+        <td>{{ product.qte > 0 ? 'Disponible' : 'Indisponible' }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+interface Product {
+  id: number;
+  label: string;
+  description: string;
+  qte: number;
+}
+
+export default defineComponent({
+  name: 'ProductList',
+  props: {
+    products: {
+      type: Array as PropType<Product[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th,
+td {
+  padding: 8px;
+  text-align: left;
+}
+</style>
